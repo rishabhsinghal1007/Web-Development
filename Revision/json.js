ca72511dbf13e961ce163eefc84dfc2f4d4b28d6
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-08-18 00:53:49 +0530

    commit

--- Revision/json.js
+++
@@ -1,8 +0,0 @@
-// Json stands for Javascript object Notation
-// it is data storage and data transmission format
-// inspired from javascript object
-
-// include strings, numbers, boolean, arrays ,objects, null
-
-// stricter than normal Object
-// all in double quotes and dont leave commas in last
